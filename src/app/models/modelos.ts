7ae77dcab061c0ed8f072c09204cde83a39c2682
NoemiBastidas <aracelybastidas@hotmail.com>
2020-11-04 07:30:44 -0500

    detalles

---
+++ src/app/models/modelos.ts
@@ -0,0 +1,16 @@
+import { ModelosComponent } from './../modelos/modelos.component';
+
+export class Model {
+    id: number;
+    controller_id: number;
+    name: string;
+    nro_inputs: number;
+    nro_outputs: number;
+
+    constructor() {
+        // this.menu = new Menu();
+        // this.template = new Catalogue();
+        // this.section = new Section();
+
+    }
+}
---
+++ src/app/models/models.index.ts
@@ -0,0 +1,3 @@
+
+// tslint:disable-next-line:eofline
+// export { Model } from './Modelos';
